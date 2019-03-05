1a3d159727270177a139a170f669e70f050b7087
tomiinek <tom@neqindi.cz>
2019-03-05 10:52:45 +0100

    Broadcast interface

---
+++ src/BikiniNinjas/Broadcast.java
@@ -0,0 +1,25 @@
+package BikiniNinjas;
+
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Broadcast {
+
+    public static void getCountOf(RobotType type){
+
+    }
+
+    public static MapLocation[] getLocations(RobotType type){
+
+
+        return null;
+    }
+
+    public static void takeIn(RobotType type, MapLocation position){
+
+    }
+
+
+
+}
