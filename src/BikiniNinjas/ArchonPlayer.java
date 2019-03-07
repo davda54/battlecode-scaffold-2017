5e7c4217494d67b3f499247fd3bfce01619561ae
Michal Pospěch <michal.pospech@gmail.com>
2019-03-07 14:33:57 +0100

    Added buying of victory points

--- src/BikiniNinjas/ArchonPlayer.java
+++ src/BikiniNinjas/ArchonPlayer.java
@@ -10,7 +10,7 @@ public class ArchonPlayer extends AbstractPlayer {
 
     private final int TREE_SENSE_RADIUS = 5;
     private final int TARGET_DISTANCE = 3;
-
+    private final int BULLET_RESERVE= 500;
     public ArchonPlayer(RobotController rc) throws GameActionException {
         super(rc);
         target = null;
@@ -52,6 +52,11 @@ public class ArchonPlayer extends AbstractPlayer {
         }
         else {
             target = null;
+        }
+        if (rc.getTeamBullets() > BULLET_RESERVE){
+            float bulletDiff = rc.getTeamBullets() - BULLET_RESERVE;
+            int victoryPointCount = (int) (bulletDiff/rc.getVictoryPointCost());
+            rc.donate(victoryPointCount * rc.getVictoryPointCost());
         }
     }
 
